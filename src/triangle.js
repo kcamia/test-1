7f5576553ff546b46cd576a8bae6827e11f55853
Kristina Camia <kcamia19@gmail.com>
2019-11-25 13:05:45 -0500

    triangle

---
+++ src/triangle.js
@@ -0,0 +1,8 @@
+const readlineSync = require("readlineSync");
+
+let base = Number(readlineSync.question("Enter base: "));
+let height = Number(readlineSync.question("Enter height: "));
+let hypo = Number(readlineSync.question("Enter hypotenuse: "));
+
+let base2 = base ** 2;
+let 
