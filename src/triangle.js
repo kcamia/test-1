fc6b9e878635e8b0327849f46228eaf2401cd07c
Kristina Camia <kcamia19@gmail.com>
2019-11-25 13:06:22 -0500

    triangle

--- src/triangle.js
+++ src/triangle.js
@@ -5,4 +5,13 @@ let height = Number(readlineSync.question("Enter height: "));
 let hypo = Number(readlineSync.question("Enter hypotenuse: "));
 
 let base2 = base ** 2;
-let 
+let height2 = height ** 2;
+let hypo2 = hypo ** 2;
+
+let sum = base2 + height2;
+
+if (sum == hypo2) {
+  console.log("\nYes, that's a right triangle!");
+} else {
+  console.log("\nNope...not a right triangle.")
+}
